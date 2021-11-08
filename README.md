f73b419586f331ea9f617ac0742b6b4b55cfbf33
Gumidafe Socorro <devgumidafe@gmail.com>
2021-11-08 17:49:55 +0000

    upload project

---
+++ README.md
@@ -0,0 +1,5 @@
+# GifExpertApp
+
+Aplicación en React con hooks para buscar y mostrar gifs animados.
+
+[Demo APP](https://gumidev.com/projects/gifExpertApp/index.html)
---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { AddCategory } from "./AddCategory";
+import { GifGrid } from "./GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <div className="header">
+      <h2 className="header-title">GifExpertApp</h2>
+
+      <AddCategory setCategories={setCategories} />
+
+      <ul className="categories">
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ul>
+      <footer className="footer">
+        Create by Gumidev. Code on{" "}
+        <a className="link" href={"https://www.google.com"} target="_blank">
+          GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
+};
